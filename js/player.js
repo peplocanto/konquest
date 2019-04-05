a5534f47c9c127c9c479f3f5a35bb94324140c26
gplocanto <gplocanto@gmail.com>
2019-04-05 14:41:43 +0200

    v.1.2

--- js/player.js
+++ js/player.js
@@ -3,8 +3,7 @@ class Player
   constructor()
   {
     this.name = ""
-    this.color = `rgba(${randomIntFromInterval(30, 200)},
-     ${randomIntFromInterval(30, 200)}, ${randomIntFromInterval(30, 200)}, 0.5)`;
+    this.color = "";
     this.active = false;
     this.ownedPlanets = 1;
   }
@@ -20,9 +19,12 @@ function createPlayers()
   {
     const newPlayer = new Player;
     newPlayer.name = `${i + 1}player`
+    newPlayer.color = colors[i]
     planets[i].color = newPlayer.color;
     planets[i].owner = newPlayer.name;
     players.push(newPlayer)
     document.querySelector(`#p${i + 1}`).style.backgroundColor = newPlayer.color;
   }
 }
+
+let colors = ["rgba(255, 0, 0, 0.3)", "rgba(0, 255, 0, 0.3)", "rgba(0, 0, 255, 0.3)", "rgba(255, 140, 0, 0.3)"]
